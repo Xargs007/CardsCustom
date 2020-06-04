7ae27221978e0cf65dcd48f7c74b45d8e25aa334
Xargs007 <65929512+Xargs007@users.noreply.github.com>
2020-06-04 01:27:08 -0400

    Add files via upload

--- script/c911000497.lua
+++ script/c911000497.lua
@@ -5,8 +5,9 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,CARD_NEOS,aux.FilterBoolFunction(function(c) return c:IsYubel(true) end))
-	--damage&recover
+	--Fusion.AddProcMix(c,true,true,CARD_NEOS,aux.FilterBoolFunction(function(c) return c:IsYubel(true) end))
+	Fusion.AddProcMix(c,true,true,CARD_NEOS,(78371393))
+	--damage&recover   or 783713930
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
